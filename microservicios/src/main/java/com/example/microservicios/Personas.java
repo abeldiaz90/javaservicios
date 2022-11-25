42c681018b002dc9daeaabe5d14ccde598907e0a
abeldiaz90 <abeldiaz90@hotmail.com>
2022-11-25 11:06:08 -0600

    se modifica la clase Personas.java

--- microservicios/src/main/java/com/example/microservicios/Personas.java
+++ microservicios/src/main/java/com/example/microservicios/Personas.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Personas {
     @RequestMapping("/greeting")
     public String greeting() {
-        return "Hola Abel";
+        return "Hola Abel buenos dias";
     }
 }
